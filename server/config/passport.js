ec45eafb74c382ff2db2b7d4023c1a3164bcfe14
lucascs20182 <lucascs20182@gmail.com>
2021-01-29 15:00:44 -0300

    protect api with passport

---
+++ server/config/passport.js
@@ -0,0 +1,26 @@
+const passport = require('passport')
+const passportJwt = require('passport-jwt')
+const { Strategy, ExtractJwt } = passportJwt
+
+const { authSecret } = require('../.env')
+
+module.exports = app => {
+    const params = {
+        secretOrKey: authSecret,
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+    }
+
+    const strategy = new Strategy(params, (payload, done) => {
+        app.db('users')
+            .where({ id: payload.id })
+            .first()
+            .then(user => done(null, user ? { ...payload } : false))
+            .catch(err => done(err, false))
+    })
+
+    passport.use(strategy)
+
+    return {
+        authenticate: () => passport.authenticate('jwt', { session: false })
+    }
+}
