18116f91e27652de7c889dcc3083419015caed29
lucascs20182 <lucascs20182@gmail.com>
2021-01-29 17:52:24 -0300

    implement soft delete for user

--- server/api/user.js
+++ server/api/user.js
@@ -41,6 +41,7 @@ module.exports = app => {
             app.db('users')
                 .update(user)
                 .where({ id: user.id })
+                .whereNull('deletedAt')
                 .then(() => res.status(204).send())
                 .catch(err => res.status(500).send(err))
         } else {
@@ -54,9 +55,39 @@ module.exports = app => {
     const get = (req, res) => {
         app.db('users')
             .select('id', 'name', 'email', 'admin')
+            .whereNull('deletedAt')
             .then(users => res.json(users))
             .catch(err => res.status(500).send(err))
     }
 
-    return { save, get }
+    const getById = (req, res) => {
+        app.db('users')
+            .select('id', 'name', 'email', 'admin')
+            .where({ id: req.params.id })
+            .whereNull('deletedAt')
+            .first()
+            .then(user => res.json(user))
+            .catch(err => res.status(500).send(err))
+    }
+
+    const remove = async (req, res) => {
+        try {
+            const articles = await app.db('articles')
+                .where({ userId: req.params.id })
+
+            notExistsOrError(articles, 'Usuário possui artigos.')
+
+            const rowsUpdated = await app.db('users')
+                .update({ deletedAt: new Date() })
+                .where( {id: req.params.id })
+            
+            existsOrError(rowsUpdated, 'Usuário não foi encontrado.')
+
+            res.status(204).send()
+        } catch(msg) {
+            res.status(400).send(msg)
+        }
+    }
+
+    return { save, get, getById, remove }
 }
