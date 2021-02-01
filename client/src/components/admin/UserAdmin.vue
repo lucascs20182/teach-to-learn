e86f45cfd0cfa0c0209ed315705445f7183a5764
lucascs20182 <lucascs20182@gmail.com>
2021-02-01 16:29:38 -0300

    update inputs to readonly depending on the mode

--- client/src/components/admin/UserAdmin.vue
+++ client/src/components/admin/UserAdmin.vue
@@ -8,6 +8,7 @@
                     <b-form-group label="Nome:" label-for="user-name">
                         <b-form-input id="user-name" type="text"
                             v-model="user.name" required
+                            :readonly="mode === 'remove'"
                             placeholder="Informe o nome do usuário" />
                     </b-form-group>
                 </b-col>
@@ -16,17 +17,18 @@
                     <b-form-group label="E-mail:" label-for="user-email">
                         <b-form-input id="user-email" type="text"
                             v-model="user.email" required
+                            :readonly="mode === 'remove'"
                             placeholder="Informe o e-mail do usuário" />
                     </b-form-group>
                 </b-col>
             </b-row>
 
             <b-form-checkbox id="user-admin" v-model="user.admin"
-                class="mt-3 mb-3">
+                v-show="user.password" class="mt-3 mb-3">
                     Administrador?
             </b-form-checkbox>
 
-            <b-row>
+            <b-row v-show="mode === 'save'">
                 <b-col md="6" sm="12">
                     <b-form-group label="Senha:" label-for="user-password">
                         <b-form-input id="user-password" type="password"
@@ -45,19 +47,23 @@
                 </b-col>
             </b-row>
 
-            <b-button variant="primary" v-if="mode === 'save'"
-                @click="save">
-                    Salvar
-            </b-button>
+            <b-row>
+                <b-col xs="12">
+                    <b-button variant="primary" v-if="mode === 'save'"
+                        @click="save">
+                            Salvar
+                    </b-button>
 
-            <b-button variant="danger" v-if="mode === 'remove'"
-                @click="remove">
-                    Excluir
-            </b-button>
+                    <b-button variant="danger" v-if="mode === 'remove'"
+                        @click="remove">
+                            Excluir
+                    </b-button>
 
-            <b-button class="ml-2" @click="reset">
-                    Cancelar
-            </b-button>
+                    <b-button class="ml-2" @click="reset">
+                            Cancelar
+                    </b-button>
+                </b-col>
+            </b-row>
         </b-form>
 
         <hr />
