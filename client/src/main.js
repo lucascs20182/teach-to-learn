dcadbdaa9fc1eec73373b27529e2bee5b4ee8109
lucascs20182 <lucascs20182@gmail.com>
2021-02-01 13:10:01 -0300

    create admin page

--- client/src/main.js
+++ client/src/main.js
@@ -8,9 +8,6 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
-// temp
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NiwibmFtZSI6IkrDo28iLCJlbWFpbCI6ImpvYW9AZ21haWwuY29tIiwiYWRtaW4iOmZhbHNlLCJpYXQiOjE2MTIxMDM3MDYsImV4cCI6MTYxMjM2MjkwNn0.EiN9UsSDQw4L5VbcAp1OagwRGePA68uN9Lv9pTfAeiE'
-
 new Vue({
   store,
   router,
