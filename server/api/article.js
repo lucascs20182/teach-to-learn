314d414181191a81fbb58735c54d042b277f59b2
lucascs20182 <lucascs20182@gmail.com>
2021-02-02 08:47:23 -0300

    increase paging limit

--- server/api/article.js
+++ server/api/article.js
@@ -49,7 +49,7 @@ module.exports = app => {
         }
     }
 
-    const limit = 3 // paginação
+    const limit = 10 // paginação
 
     const get = async (req, res) => {
         const page = req.query.page || 1
